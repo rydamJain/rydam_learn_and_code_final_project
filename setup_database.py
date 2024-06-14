2f691dba1d9e3e7291bf497542ff9a552d1cdaab
rydamjain01 <rydam.jain@intimetec.com>
2024-06-14 11:00:51 +0530

    modified user_services

--- setup_database.py
+++ setup_database.py
@@ -3,19 +3,18 @@ import sqlite3
 class DatabaseServices:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()  # Initialize the cursor here
         self.create_tables()
 
     def create_tables(self):
-        cursor = self.conn.cursor()
-        
-        cursor.execute('''
+        self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS roles (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL
         )
         ''')
         
-        cursor.execute('''
+        self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             email TEXT NOT NULL,
@@ -27,15 +26,13 @@ class DatabaseServices:
         self.conn.commit()
 
     def execute(self, query, params=()):
-        cursor = self.conn.cursor()
-        cursor.execute(query, params)
+        self.cursor.execute(query, params)
         self.conn.commit()
-        return cursor
+        return self.cursor
 
     def fetchall(self, query, params=()):
-        cursor = self.conn.cursor()
-        cursor.execute(query, params)
-        return cursor.fetchall()
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
 
     def close(self):
         self.conn.close()
--- user_services.py
+++ user_services.py
@@ -9,15 +9,26 @@ class UserServices:
         INSERT INTO users (id, email, role_id) VALUES (?, ?, ?)
         ''', (id, email, role_id))
 
+    def delete_user(self, id):
+        self.db.execute('''
+        DELETE FROM users WHERE id = ?
+        ''', (id,))
+
     def fetch_users(self):
         return self.db.fetchall('SELECT * FROM users')
 
+# Initialize the database and user services
 db = DatabaseServices('recommendation_engine.db')
 user_services = UserServices(db)
-# user_services.insert_user(1,"rydam@gmail.com",1)
-# user_services.insert_user(2,"hemish@gmail.com",2)
-# user_services.insert_user(3,"nidhi@gmail.com",3)
-# user_services.insert_user(4,"ahaana@gmail.com",3)
-# user_services.insert_user(6,"john@gmail.com",3)
-# user_services.insert_user(5,"naman@gmail.com",3)
-print(user_services.fetch_users())
+
+# Uncomment the following lines to insert users into the database
+# user_services.insert_user(1, "rydam@gmail.com", 1)
+# user_services.insert_user(2, "hemish@gmail.com", 2)
+# user_services.insert_user(3, "nidhi@gmail.com", 3)
+# user_services.insert_user(4, "ahaana@gmail.com", 3)
+# user_services.insert_user(6, "john@gmail.com", 3)
+# user_services.insert_user(5, "naman@gmail.com", 3)
+# user_services.delete_user(5)
+# Fetch and print all users
+users = user_services.fetch_users()
+print(users)
