a14e01f0187e3d7770313c3ee37bd0cb5f496793
rydamjain01 <rydam.jain@intimetec.com>
2024-06-17 08:46:03 +0530

    role_service modified

--- role_service.py
+++ role_service.py
@@ -10,24 +10,19 @@ class RoleServices:
         ''', (role_id, name))
 
     def delete_role(self, role_id):
-        self.db.execute(f'''
-        DELETE FROM  roles  WHERE id = {role_id}
-        ''')
+        self.db.execute('''
+        DELETE FROM roles WHERE id = ?
+        ''', (role_id,))
 
     def fetch_roles(self):
-        cursor = self.db.conn.cursor()
-        cursor.execute('SELECT * FROM roles')
-        roles = cursor.fetchall()
-        return roles
+        return self.db.fetchall('SELECT * FROM roles')
 
     def get_role_id_by_email(self, email):
-        cursor = self.db.conn.cursor()
-        cursor.execute('''
+        result = self.db.fetchall('''
         SELECT role_id FROM users WHERE email = ?
         ''', (email,))
-        result = cursor.fetchone()
         if result:
-            return result[0]
+            return result[0][0]
         return None
 
 db = DatabaseServices('recommendation_engine.db')
@@ -36,3 +31,4 @@ role_services = RoleServices(db)
 # role_services.insert_role( 3, "employee")
 # role_services.delete_role(2)
 print(role_services.fetch_roles())
+print(role_services.get_role_id_by_email("hemish@gmail.com"))
