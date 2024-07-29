1579551b8fcda181081c9f1fa911064c4730bdfa
rydamjain01 <rydam.jain@intimetec.com>
2024-07-29 11:54:18 +0530

    admin module refactored

--- services/admin.py
+++ services/admin.py
@@ -4,7 +4,9 @@ from services.item_service import ItemServices
 
 class AdminHandler:
     def __init__(self, database):
+        self.database = database
         self.item_services = ItemServices(database)
+       
 
     def show_admin_options(self):
         options = """
@@ -19,41 +21,53 @@ class AdminHandler:
 
     def handle_choice(self, conn, choice):
         if choice == '1':
-            conn.sendall("Enter item id:".encode())
-            item_id = int(conn.recv(1024).decode().strip())
-
-            conn.sendall("Enter item name:".encode())
-            name = conn.recv(1024).decode().strip()
-
-            conn.sendall("Enter item price:".encode())
-            price = float(conn.recv(1024).decode().strip())
-
-            conn.sendall("Enter meal type id:".encode())
-            meal_type_id = int(conn.recv(1024).decode().strip())
-
-            conn.sendall("Enter availability status (1 for available, 0 for not available):".encode())
-            availability_status = bool(int(conn.recv(1024).decode().strip()))
-
-            # Call the appropriate method from ItemServices to add the item
-            self.item_services.insert_into_items([(item_id, name, price, meal_type_id, availability_status)])
-            conn.sendall("Item added successfully.".encode())
-
+            self.add_item(conn)
         elif choice == '2':
-            conn.sendall("Enter item id:".encode())
-            item_id = int(conn.recv(1024).decode().strip())
-            self.item_services.delete_item(item_id)
-            conn.sendall("Item deleted successfully.".encode())
+            self.delete_item(conn)
         elif choice == '3':
-            conn.sendall("Enter item id to update availability status:".encode())
-            item_id = int(conn.recv(1024).decode().strip())
-
-            conn.sendall("Enter new availability status (1 for available, 0 for not available):".encode())
-            availability_status = bool(int(conn.recv(1024).decode().strip()))
-
-            self.item_services.update_availability_status(item_id, availability_status)
-            conn.sendall("Item availability status updated successfully.".encode())
-            
+            self.update_item(conn)
         elif choice == '4':
-            print("See responses from employees")
+            self.view_feedbacks(conn)
         else:
             conn.sendall("Invalid choice. Please try again.".encode())
+
+    def add_item(self, conn):
+        conn.sendall("Enter item id:".encode())
+        item_id = int(conn.recv(1024).decode().strip())
+
+        conn.sendall("Enter item name:".encode())
+        name = conn.recv(1024).decode().strip()
+
+        conn.sendall("Enter item price:".encode())
+        price = float(conn.recv(1024).decode().strip())
+
+        conn.sendall("Enter meal type id:".encode())
+        meal_type_id = int(conn.recv(1024).decode().strip())
+
+        conn.sendall("Enter availability status (1 for available, 0 for not available):".encode())
+        availability_status = bool(int(conn.recv(1024).decode().strip()))
+
+        # Call the appropriate method from ItemServices to add the item
+        self.item_services.insert_into_items([(item_id, name, price, meal_type_id, availability_status)])
+        conn.sendall("Item added successfully.".encode())
+
+    def delete_item(self, conn):
+        conn.sendall("Enter item id:".encode())
+        item_id = int(conn.recv(1024).decode().strip())
+        self.item_services.delete_item(item_id)
+        conn.sendall("Item deleted successfully.".encode())
+
+    def update_item(self, conn):
+        conn.sendall("Enter item id to update availability status:".encode())
+        item_id = int(conn.recv(1024).decode().strip())
+
+        conn.sendall("Enter new availability status (1 for available, 0 for not available):".encode())
+        availability_status = bool(int(conn.recv(1024).decode().strip()))
+
+        self.item_services.update_availability_status(item_id, availability_status)
+        conn.sendall("Item availability status updated successfully.".encode())
+
+    def view_feedbacks(self, connection):
+        feedbacks = self.database.fetchall("SELECT * FROM feedback")
+        for feedback in feedbacks:
+            connection.sendall(f"Item ID: {feedback[1]}, User ID: {feedback[2]}, Rating: {feedback[3]}, Comment: {feedback[4]},  Sentiment Score: {feedback[5]}\n".encode())
